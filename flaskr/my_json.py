06040e241692d3759799efd3df474a7fdaeb5ccb
ViniMSLima <vinisary.lima@gmail.com>
2024-08-26 10:14:18 -0300

    now receiving file instead of path

--- flaskr/my_json.py
+++ flaskr/my_json.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from flask import Blueprint, request, jsonify
 import numpy as np
 import tensorflow as tf
@@ -10,18 +11,22 @@ maybeResults = ["bad_blue", "bad_red", "good_blue", "good_red"]
 
 @bp.route('/', methods=['POST'])
 def process_images():
-    data = request.get_json()
-    nImages = len(data['images'])
-    wordResults = []
-
-    for img_path in data['images']:
-        img_data = np.array([tf.keras.utils.load_img(img_path)])
-        wordResults.append(model.predict(img_data))
-
-    results = ""
-    for i in range(nImages):
-        results += maybeResults[np.argmax(wordResults[i][0])]
-
-    print(results)
+    if 'image' not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
     
-    return jsonify(results)
+    image_file = request.files['image']
+
+    # Converter o arquivo para um objeto BytesIO
+    image_stream = BytesIO(image_file.read())
+    
+    # Convertendo a imagem para o formato necessário para a previsão
+    img = tf.keras.utils.img_to_array(tf.keras.utils.load_img(image_stream, target_size=(224, 224)))  # Ajuste o target_size conforme necessário
+    img = np.expand_dims(img, axis=0)
+    
+    # Realizando a predição com o modelo
+    predictions = model.predict(img)
+    
+    # Convertendo predições para uma resposta
+    result = maybeResults[np.argmax(predictions[0])]
+    
+    return jsonify(result)
